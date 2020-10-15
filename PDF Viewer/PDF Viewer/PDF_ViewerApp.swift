92ad9fe60623d3a2cc62cfbaf0677099f35fc392
Jeremy Kemery <jeremykemery@gmail.com>
2020-10-14 22:51:48 -0500

    created project

---
+++ PDF Viewer/PDF Viewer/PDF_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDF_ViewerApp.swift
+//  PDF Viewer
+//
+//  Created by Jeremy Kemery on 10/14/20.
+//
+
+import SwiftUI
+
+@main
+struct PDF_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
