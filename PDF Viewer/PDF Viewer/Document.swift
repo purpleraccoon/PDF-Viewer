b93e5d32f3052a1fbc08c4b43ebee81bed449eb5
Jeremy Kemery <jeremykemery@gmail.com>
2020-10-14 23:08:01 -0500

    new document based app

---
+++ PDF Viewer/PDF Viewer/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  PDF Viewer
+//
+//  Created by Jeremy Kemery on 10/14/20.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
