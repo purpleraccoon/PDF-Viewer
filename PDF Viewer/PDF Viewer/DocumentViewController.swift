1987a22556b40a8ac67a2c461560aec3e9b66d50
Jeremy Kemery <jeremykemery@gmail.com>
2020-10-15 11:12:14 -0500

    pdfview

--- PDF Viewer/PDF Viewer/DocumentViewController.swift
+++ PDF Viewer/PDF Viewer/DocumentViewController.swift
@@ -6,21 +6,27 @@
 //
 
 import UIKit
+import PDFKit
 
 class DocumentViewController: UIViewController {
     
-    @IBOutlet weak var documentNameLabel: UILabel!
+	@IBOutlet weak var pdfView: PDFView!
+	@IBOutlet weak var documentNameLabel: UILabel!
     
-    var document: UIDocument?
+    var document: Document?
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+
+		pdfView.displayDirection = .horizontal
+		pdfView.displayMode = .singlePage
         
         // Access the document
         document?.open(completionHandler: { (success) in
             if success {
                 // Display the content of the document, e.g.:
                 self.documentNameLabel.text = self.document?.fileURL.lastPathComponent
+				self.pdfView.document = self.document!.pdfDocument
             } else {
                 // Make sure to handle the failed import appropriately, e.g., by presenting an error message to the user.
             }
